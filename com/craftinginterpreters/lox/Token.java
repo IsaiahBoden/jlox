5c4f2cde33b9f5afbadc8e9bdb70fdf49f11edc9
Isaiah <imboden25@protonmail.com>
2023-05-09 16:11:59 -0400

    Expr.java updated

--- com/craftinginterpreters/lox/Token.java
+++ com/craftinginterpreters/lox/Token.java
@@ -8,10 +8,10 @@ class Token {
 
 	Token(TokenType type, String lexeme, Object literal, int line) {
   	this.type = type;
-		this.lexeme = lexeme;
-		this.literal = literal;
-		this.line = line;
-	}
+	this.lexeme = lexeme;
+	this.literal = literal;
+	this.line = line;
+}
 	
 	public String toString() {
 		return type + " " + lexeme + " " + literal;
